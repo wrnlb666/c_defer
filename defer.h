395704b898b7cafa7b8e10cdea7b71c94b4556bd
wrnlb <103810793+wrnlb666@users.noreply.github.com>
2023-07-21 05:22:15 -0400

    change namespace

--- defer.h
+++ defer.h
@@ -2,11 +2,11 @@
 #define __DEFER_H__
 
 
-#define _concat(x, y) x ## y
-#define concat(x, y) _concat(x, y)
+#define _defer_concat(x, y) x ## y
+#define defer_concat(x, y) _defer_concat(x, y)
 #define defer(block)     \
-void concat( defer_func, __LINE__ )(){block} \
-__attribute__((cleanup(concat( defer_func, __LINE__ )))) char concat( defer_var, __LINE__ )
+void defer_concat( defer_func, __LINE__ )(){block} \
+__attribute__((cleanup(defer_concat( defer_func, __LINE__ )))) char defer_concat( defer_var, __LINE__ )
 
 
 #endif  // __DEFER_H__
