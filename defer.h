3e77d7b0e0528e33ddd0a860037b922953c80827
wrnlb <103810793+wrnlb666@users.noreply.github.com>
2024-03-22 11:11:28 +0800

    Added clang support via clang blocks

--- defer.h
+++ defer.h
@@ -4,9 +4,26 @@
 
 #define _defer_concat(x, y) x ## y
 #define defer_concat(x, y) _defer_concat(x, y)
-#define defer(block)     \
-void defer_concat( defer_func, __LINE__ )(){block;} \
-__attribute__((cleanup(defer_concat( defer_func, __LINE__ )))) char defer_concat( defer_var, __LINE__ )
+
+#if defined __clang__
+#include <Block.h>
+typedef void (^defer_clang_type)(void);
+static void defer_clang_wrapper(void* fn) {
+    defer_clang_type* func = (defer_clang_type*)fn;
+    (*func)();
+}
+#define defer(block)    \
+__attribute__((cleanup(defer_clang_wrapper)))   \
+defer_clang_type defer_concat(defer_func, __LINE__) = ^{block;};    \
+(void) defer_concat(defer_func, __LINE__);
+#elif defined __GNUC__  //  __clang__
+#define defer(block)    \
+void defer_concat(defer_func, __LINE__)(){block;};  \
+__attribute__((cleanup(defer_concat(defer_func, __LINE__))))    \
+char defer_concat(defer_var, __LINE__);
+#else   // __GNUC__
+#error This can only be used with gcc or clang
+#endif  // other compiler
 
 
 #endif  // __DEFER_H__
